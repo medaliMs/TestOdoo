19ff014bd1b3bb4e8d3412dd90a62263d4944963
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-03-08 05:31:03 -0500

    reduce the for loop in the cron

--- models/inherit_model.py
+++ models/inherit_model.py
@@ -14,17 +14,15 @@ class UserStory(models.Model):
     @api.model
     def add(self):
         print(tuleap_data.get_Data())
-
-        item_to_insert_or_update = [
-            {'date': None, 'user_story_id': None, 'employee_id': None, 'project_id': None, 'task_id': False,
-             'name': None, 'unit_amount': None, 'user_id': False}]
+        items_to_create = []
 
         # item_from_script = tuleap_data.get_Data()
-        item_from_script = [{'userstory_id': '295735', 'userstory_title': 'Gestion utilisateurs', 'project_id': '4571', 'project_name': 'SNCF Courrier', 'user_id': 3571, 'time_spent': 2},
-                            {'userstory_id': '295736', 'userstory_title': 'Gestion utilisateurs', 'project_id': '4571', 'project_name': 'SNCF Courrier', 'user_id': 3570, 'time_spent': 1}]
-        bool = False
+        items_from_script = [{'userstory_id': '295735', 'userstory_title': 'Gestion utilisateurs', 'project_id': '4571',
+                              'project_name': 'SNCF Courrier', 'user_id': 3571, 'time_spent': 2},
+                             {'userstory_id': '295736', 'userstory_title': 'Gestion utilisateurs', 'project_id': '4571',
+                              'project_name': 'SNCF Courrier', 'user_id': 3570, 'time_spent': 1}]
         print(self)
-        for item in item_from_script:
+        for item in items_from_script:
             item_to_search = self.search([('user_story_id', '=', item['userstory_id'])])
             print(item_to_search)
             if item_to_search:
@@ -48,14 +46,11 @@ class UserStory(models.Model):
                     print("project not found")
                     continue
 
-                item_to_insert_or_update[0]["date"] = datetime.today().strftime('%Y-%m-%d')
-                item_to_insert_or_update[0]["user_story_id"] = item['userstory_id']
-                item_to_insert_or_update[0]["employee_id"] = employee.id
-                item_to_insert_or_update[0]["project_id"] = project.id
-                item_to_insert_or_update[0]["name"] = item['userstory_title']
-                item_to_insert_or_update[0]["unit_amount"] = item['time_spent']
-                print(item_to_insert_or_update[0])
-                item.write(item_to_insert_or_update[0])
+                item_to_update = {'date': datetime.today().strftime('%Y-%m-%d'), 'user_story_id': item['userstory_id'],
+                                  'employee_id': employee.id, 'project_id': project.id, 'task_id': False,
+                                  'name': item['userstory_title'], 'unit_amount': item['time_spent'], 'user_id': False}
+                print(item_to_update)
+                item.write(item_to_update)
             else:
                 project = self.env['project.project'].search([('alm_project_id', '=', item['project_id'])])
                 employee = self.env['hr.employee'].search([('alm_user_id', '=', item['user_id'])])
@@ -69,16 +64,27 @@ class UserStory(models.Model):
                     # raise ValidationError("project not found please create it first")
                     print("project not found")
                     continue
+                item_to_insert = {'date': datetime.today().strftime('%Y-%m-%d'), 'user_story_id': item['userstory_id'],
+                                  'employee_id': employee.id, 'project_id': project.id, 'task_id': False,
+                                  'name': item['userstory_title'], 'unit_amount': item['time_spent'], 'user_id': False}
+                # item_to_insert_or_update[0]["date"] = '2021-05-13'
+                # item_to_insert_or_update[0]["user_story_id"] = item['userstory_id']
+                # item_to_insert_or_update[0]["employee_id"] = employee.id
+                # item_to_insert_or_update[0]["project_id"] = project.id
+                # item_to_insert_or_update[0]["name"] = item['userstory_title']
+                # item_to_insert_or_update[0]["unit_amount"] = item['time_spent']
+                # print(item_to_insert_or_update[0])
 
-                item_to_insert_or_update[0]["date"] = '2021-05-13'
-                item_to_insert_or_update[0]["user_story_id"] = item['userstory_id']
-                item_to_insert_or_update[0]["employee_id"] = employee.id
-                item_to_insert_or_update[0]["project_id"] = project.id
-                item_to_insert_or_update[0]["name"] = item['userstory_title']
-                item_to_insert_or_update[0]["unit_amount"] = item['time_spent']
-                print(item_to_insert_or_update[0])
+                # item_to_insert["date"] = '2021-05-13'
+                # item_to_insert["user_story_id"] = item['userstory_id']
+                # item_to_insert["employee_id"] = employee.id
+                # item_to_insert["project_id"] = project.id
+                # item_to_insert["name"] = item['userstory_title']
+                # item_to_insert["unit_amount"] = item['time_spent']
+                # print(item_to_insert)
+                items_to_create.append(item_to_insert)
 
-                self.create(item_to_insert_or_update)
+        self.create(item_to_insert_or_update)
 
 
 class AlmProject(models.Model):
